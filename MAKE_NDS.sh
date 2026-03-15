1293fcb890fbaba4f15e0cdefb5d95c25d74b3ef
brendan <bscarell@gmail.com>
2026-03-15 05:14:13 -0400

    running make now autorun MAKE_NDS

--- MAKE_NDS.sh
+++ MAKE_NDS.sh
@@ -1,2 +1,1 @@
-echo "this is from the original flashme source"
 ndstool -c flashme.nds -7 flashme.arm7 -9 flashme.arm9 -r9 0x2004000 -r7 0x2380000 -b icon.bmp "FlashMe;http://ds.gcdev.com/dsfirmware"
